0d253f3ccbe246900b56422df6c64d326e8369c8
Justin Reppert <10255098+reppertj@users.noreply.github.com>
2022-07-12 16:50:04 -0500

    do 80-20 split

--- src/templates/adherence/adherence.py
+++ src/templates/adherence/adherence.py
@@ -4,6 +4,27 @@ from typing import List, Tuple
 
 import datasets
 
+TEST_PAPERS = {
+    "vittengl-2009.pdf",
+    "budtz-jorgensen-2006.pdf",
+    "agberotimi-2021.pdf",
+    "deutschmann-2019.pdf",
+    "chakravorty-bedy-2019.pdf",
+    "abebe-2018.pdf",
+    "vuuren-cherney-2014.pdf",
+    "cheng-2020.pdf",
+    "idinsight-2020.pdf",
+    "awasthi-2013.pdf",
+    "miguel-kremer-2004.pdf",
+    "dicko-2008.pdf",
+    "routledge-2006.pdf",
+    "nyqvist-218.pdf",
+    "matangila-2015.pdf",
+    "keenan-2018.pdf",
+    "bassi-2018.pdf",
+    "smithuis-2013.pdf",
+}
+
 
 _DESCRIPTION = """\
 Whether a sentence from a research paper is about take-up/adherence/compliance or not."""
@@ -29,6 +50,7 @@ class AdherenceDataset(datasets.GeneratorBasedBuilder):
                 "section": datasets.Value("string"),
                 "prediction": datasets.Value("float"),
                 "intervention": datasets.Value("string"),
+                "document_id": datasets.Value("string"),
             }
         )
 
@@ -56,8 +78,30 @@ class AdherenceDataset(datasets.GeneratorBasedBuilder):
         ]
 
     def _generate_examples(self, split):
-        filename = "train.json" if split == datasets.Split.TRAIN else "test.json"
-        yield from _generate_from_file(os.path.join(self.data_dir, filename), split)
+        source_files = ("train.json", "test.json")
+        all_examples = []
+        for file in source_files:
+            all_examples.extend(list(_generate_from_file(os.path.join(self.data_dir, file), split)))
+        
+        positive_examples = [example for example in all_examples if example["label"] == 1]
+        negative_examples = [example for example in all_examples if example["label"] == 0]
+
+        if datasets.Split.TRAIN == split:
+            # get 80% of both positive and negative examples
+            positive_train = positive_examples[: int(len(positive_examples) * 0.8)]
+            negative_train = negative_examples[: int(len(negative_examples) * 0.8)]
+            # shuffle for added security in case the training order isn't random
+            train = positive_train + negative_train
+            print(f"Training on {len(train)} examples")
+            yield from ((idx, example[1]) for idx, example in enumerate(train))
+
+        else:
+            # train on the remaining 20% of positive and negative examples
+            positive_val = positive_examples[int(len(positive_examples) * 0.8) :]
+            negative_val = negative_examples[int(len(negative_examples) * 0.8) :]
+            val = positive_val + negative_val
+            print(f"Validation on {len(val)} examples")
+            yield from ((idx, example[1]) for idx, example in enumerate(val))
 
 
 def _generate_from_file(filename: str, split):
@@ -66,7 +110,7 @@ def _generate_from_file(filename: str, split):
 
     rows: List[Tuple[int, dict]] = []
 
-    for key, row in enumerate(data):
+    for row in data:
         label = (
             1
             if row["annotations"]
@@ -76,15 +120,15 @@ def _generate_from_file(filename: str, split):
             .get("choices", [""])[0] in ("adherence", "1")
             else 0
         )
-        rows.append((key, {
+        rows.append({
             "sentence": row["data"]["sentence"],
             "context": row["data"]["context"],
             "intervention": row["data"]["intervention"],
             "section": row["data"]["section"],
+            "document_id": row["data"]["document_id"],
             "prediction": row["data"].get("base_prediction") or row["data"]["prediction"],
             "label": label,
-        }))
+        })
 
     for row in rows:
         yield row
-           
